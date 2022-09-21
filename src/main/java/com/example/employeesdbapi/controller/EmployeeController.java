951938379a8982ded7c3223756326d086abb31cf
Athanasios <asal@iti.gr>
2022-09-21 16:23:45 +0300

    1. Updated the EmployeeController. 2. Put the MySQL db details in the application.properties file. 3. There is an issue with Maven build - to fix

--- src/main/java/com/example/employeesdbapi/controller/EmployeeController.java
+++ src/main/java/com/example/employeesdbapi/controller/EmployeeController.java
@@ -5,10 +5,14 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.employeesdbapi.model.Employee;
 import com.example.employeesdbapi.repository.EmployeeReporitory;
 
+@RestController
+@RequestMapping(path = "/api")
 public class EmployeeController {
 
 	@Autowired
