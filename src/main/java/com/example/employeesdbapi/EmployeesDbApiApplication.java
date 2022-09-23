9bd328e4af2636f68fdff7517c438dd7fb5bec93
Athanasios <asal@iti.gr>
2022-09-23 13:15:27 +0300

    1. Explanatory comments in pom.xml. 2. Remove import statement in application class.

--- src/main/java/com/example/employeesdbapi/EmployeesDbApiApplication.java
+++ src/main/java/com/example/employeesdbapi/EmployeesDbApiApplication.java
@@ -2,7 +2,6 @@ package com.example.employeesdbapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class EmployeesDbApiApplication {
